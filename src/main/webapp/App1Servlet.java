762632e2f2eb87d23422c5848eb7299d421ab558
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-30 20:20:00 +0900

    App1Servlet.javaを作成する

--- src/main/webapp/App1Servlet.java
+++ src/main/webapp/App1Servlet.java
@@ -1,7 +1,19 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+//HttpServletを継承して作る
 public class App1Servlet extends HttpServlet {
     praivate static final long serialVersionUID = 1L;
+
+    //スーパークラス(HttpServlet)のコンストラクタを呼び出す
+    public App1Servlet() {
+        super();
+    }
+
 
 
 }
