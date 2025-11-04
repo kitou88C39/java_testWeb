d11534cddbcffc00ec51989e65cf7ee9800d4789
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-04 15:29:35 +0900

    app4.javaを作成する

---
+++ src/main/webapp/WEB-INF/app4.java
@@ -0,0 +1,40 @@
+package yurufuwa.prog.sample;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//HttpServletを継承して作る
+public class App4 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // パラメータ取得
+        String str1 = request.getParameter("str1");
+        String str2 = request.getParameter("str2");
+
+        // モデルで文字列結合
+        Concat c = new Concat();
+        c.setStr1(str1);
+        c.setStr2(str2);
+        c.join();
+
+        // モデルの結果クラスを取得
+        ConcatResult result = c.getResult();
+
+        // モデルをリクエストにセット
+        request.setAttribute("result", c);
+
+        // ビュー(JSP)に結果を出力してもらう
+        ServletContext ctx = getServletContext();
+        RequestDispatcher rd = ctx.getRequestDispatcher("/WEB-INF/join.jsp");
+        rd.forward(request, response);
+    }
+}
